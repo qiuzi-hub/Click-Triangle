127ae5c0f1c40c8cd097ed80aa31021dff7873fb
BBQiu <3315581510@qq.com>
2024-10-03 17:02:44 +0800

    Click

---
+++ Two D-06/Assets/Triangle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Triangle : MonoBehaviour
+{
+    public float maxHealth=100;//公共的最大血条
+    private float health=100;//自己私有血条，字段小写字母开头
+
+    //血量改变的时候发生这件事
+    public UnityEvent<float> HpChangedEvent;
+
+    //对字段进行访问
+    //封装属性的目的
+    //①在外可以访问，但是不能访问字段
+    //通过属性修改字段，可以知道每一次血量的值
+    public float Health {//属性 ，大写字母开头
+        //get，set两个方法
+        get => health;
+        set
+        //方法体
+        {
+            if(value>0 )
+                health = value;
+            HpChangedEvent?.Invoke(health / maxHealth); //传的是比例所以用/
+        }
+    }
+
+    //因为点击鼠标左键的时候会减少血条，而血条是在Canvas下的
+    //所以在Canvas下添加脚本
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Health -= 10;//血条每次减十，也可以小写的helath
+            if (health < 0)//当小于0
+            {
+                Health = maxHealth;//回到最大血条
+            }
+            //Health = Health % maxHealth;//为啥要求余
+        }
+ 
+    }
+}
---
+++ Two D-06/Assets/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider HpSilder;//获取血条滑块
+    public GameObject tri;
+    public TMPro.TMP_Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HpSilder.value = HpSilder.maxValue;//一开始血条的值是最大的
+        if (tri == null)//如果忘记将三角形拖进去，可以用null进行判断
+        {
+            tri = GameObject.FindGameObjectWithTag("Player");//也可以通过player查找
+        }
+        tri.GetComponent<Triangle>().HpChangedEvent.AddListener(UpdateHpSilder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //更新血条
+    void UpdateHpSilder(float value) {
+        HpSilder.value = value;//将更新的value值传给血条
+    
+    }
+}
